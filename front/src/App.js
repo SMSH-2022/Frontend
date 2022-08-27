2e984544dfabf1691d8218eab41a149ea60da275
Suyeon <syhhwang1231@gmail.com>
2022-08-28 04:51:17 +0900

    Add: MainPage, ArticleBoard page

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,18 @@
+import React from "react";
+import MainPage from "./routes/MainPage";
+import PostList from "./routes/PostList";
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<MainPage/>} />
+      <Route exact path="/board/:category" element={<PostList/>}/>
+    </Routes>
+    </BrowserRouter>
+    
+    // <MainPage/>
   );
 }
 
